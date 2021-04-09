51d67d2ff56ca30a5e273502752ee2ca8a3b26fc
nohssam <nohssam73@gmail.net>
2021-04-09 16:16:30 +0900

    i/o_end

--- src/com/ict07/IO/Ex27_Input.java
+++ src/com/ict07/IO/Ex27_Input.java
@@ -5,12 +5,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
-import java.util.Scanner;
 
-public class Ex25_Input {
+public class Ex27_Input {
 	public static void main(String[] args) {
-		String pathname = "c:"+File.separator+"study"
-						+File.separator+"util"+File.separator+"ict09.ser";
+		String pathname = "c:" + File.separator + "study" + File.separator 
+							+ "util" + File.separator + "ict11.ser";
 		File file = new File(pathname);
 		FileInputStream fis = null;
 		BufferedInputStream bis = null;
@@ -20,34 +19,25 @@ public class Ex25_Input {
 			bis = new BufferedInputStream(fis);
 			ois = new ObjectInputStream(bis);
 			
-			ArrayList<Ex25_VO> list = (ArrayList<Ex25_VO>)ois.readObject();
-			/*
-			System.out.println("이름\t총점\t평균\t학점");
-			for (Ex25_VO k : list) {
-				System.out.print(k.getName()+"\t" );
-				System.out.print(k.getSum()+"\t" );
-				System.out.print(k.getAvg()+"\t" );
-				System.out.println(k.getHak());
-			}
-			*/
-			//  정렬(?)
-			// 컬렉션을 배열로 변경해서 정렬
-			Ex25_VO[]  arr = (Ex25_VO[]) list.toArray(new Ex25_VO[0]);
-		
-			Ex25_VO tmp = new Ex25_VO();  // 임시저장
-		
-			for (int i = 0; i < arr.length-1; i++) {
-				for (int j = i+1; j < arr.length; j++) {
-					if(arr[i].getSum() < arr[j].getSum()) {
-						tmp = arr[i];
-						arr[i] = arr[j];
-						arr[j] = tmp;
-					}
+			ArrayList<Ex27_VO> list = (ArrayList<Ex27_VO>)ois.readObject();
+			
+			// 총점,평균,학점 구하기
+			for (Ex27_VO k : list) {
+				k.setSum(k.getKor()+k.getEng()+k.getMath());
+				k.setAvg((int)(k.getSum()/3.0*10)/10.0);
+				if(k.getAvg()>=90) {
+					k.setHak("A학점");
+				}else if(k.getAvg()>=80) {
+					k.setHak("B학점");
+				}else if(k.getAvg()>=70) {
+					k.setHak("C학점");
+				}else {
+					k.setHak("F학점");
 				}
 			}
-		
+			
 			System.out.println("이름\t총점\t평균\t학점");
-			for (Ex25_VO k : arr) {
+			for (Ex27_VO k : list) {
 				System.out.print(k.getName()+"\t" );
 				System.out.print(k.getSum()+"\t" );
 				System.out.print(k.getAvg()+"\t" );
