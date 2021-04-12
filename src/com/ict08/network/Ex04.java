9b0ec779ec2aa8dd1cabf994b23baae152f09a73
nohssam <nohssam73@gmail.net>
2021-04-12 12:24:31 +0900

    network

--- src/com/ict08/network/Ex04.java
+++ src/com/ict08/network/Ex04.java
@@ -4,40 +4,35 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 
-public class Ex02 {
+public class Ex04 {
 	public static void main(String[] args) {
-		// URL 클래스 : URL(Uniform Resource Locator) : 접근 가능한 자원의 주소를 말한다.
-		//              final 클래스이기 때문에 상속 안됨, 
-		InputStream is = null;
-		InputStreamReader isr = null;
-		BufferedReader br = null;
-		
-		String pathname = "c:"+File.separator+"study"+File.separator+"util"+File.separator+"기상청.txt";
+		String pathname = "c:"+File.separator+"study"+
+							File.separator+"util"+File.separator+"도서관.txt";
 		File file = new File(pathname);
 		FileWriter fw = null;
 		BufferedWriter bw = null;
+		
+		BufferedReader br = null;
 		try {
-			// 먼저 웹페이지를 읽자
-			URL url = new URL("https://www.kma.go.kr/XML/weather/sfc_web_map.xml");
-			is = url.openStream();
-			isr = new InputStreamReader(is,"utf-8"); // "utf-8"은 한글처리를 말한다.
-			br = new BufferedReader(isr);
+			URL url = new URL("http://openapi.seoul.go.kr:8088/sample/json/SeoulLibraryTime/1/5/");
+			URLConnection conn = url.openConnection();
+			br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
 			
 			String msg = null;
 			StringBuffer sb = new StringBuffer();
-			while ((msg = br.readLine())!= null) {
+			while((msg=br.readLine())!= null) {
 				sb.append(msg+"\n");
 			}
 			System.out.println(sb.toString());
+			
 			fw = new FileWriter(file);
 			bw = new BufferedWriter(fw);
 			bw.write(sb.toString());
 			bw.flush();
-			
 		} catch (Exception e) {
 			System.out.println(e);
 		} finally {
@@ -45,8 +40,6 @@ public class Ex02 {
 				bw.close();
 				fw.close();
 				br.close();
-				isr.close();
-				is.close();
 			} catch (Exception e2) {
 				System.out.println(e2);
 			}
